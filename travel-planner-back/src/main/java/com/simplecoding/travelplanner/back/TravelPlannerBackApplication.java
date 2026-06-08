d62f91b6018fcb2b1ad3a702d35cf6e4c5fc7173
sunterx1-afk <sunterx1@gmail.com>
2026-06-08 16:55:04 +0900

    백 패키지
    백 패키지

---
+++ travel-planner-back/src/main/java/com/simplecoding/travelplanner/back/TravelPlannerBackApplication.java
@@ -0,0 +1,13 @@
+package com.simplecoding.travelplanner.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelPlannerBackApplication.class, args);
+    }
+
+}
---
+++ travel-planner-back/src/test/java/com/simplecoding/travelplanner/back/TravelPlannerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplecoding.travelplanner.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
