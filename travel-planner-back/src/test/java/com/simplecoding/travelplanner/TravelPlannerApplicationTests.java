c1d58242a3426294609e560e9455774e2422c5d8
sunterx1-afk <sunterx1@gmail.com>
2026-06-08 16:39:07 +0900

    백엔드 패키지
    백엔드 패키지

---
+++ travel-planner-back/src/test/java/com/simplecoding/travelplanner/TravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplecoding.travelplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
